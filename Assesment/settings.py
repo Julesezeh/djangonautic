f6526e5450a926757a8502574c31f82c6de133c4
Jules Ezeh <julesezeh23@gmail.com>
2022-09-13 19:23:14 +0100

    new database

--- Assesment/settings.py
+++ Assesment/settings.py
@@ -26,7 +26,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-jf9n9!1!hk+&ky27q2wqvz=lfuh+*xkq@j=w)du0hgqw$egkro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
